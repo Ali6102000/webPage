67064c93684d7e1ecb53ba15db094af8a02ee367
Ali Olleik <ali.oleik.2000@gmail.com>
2024-08-03 00:10:32 +0300

    c2

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function getStudents(){
     fetch('https://trainingapi-3wic.onrender.com/students')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => {
+        var users=document.getElementById("select_users");
+        json.forEach(element => {
+          var _opt=document.createElement("option");
+          _opt.innerHTML=element.name;
+          users.appendChild(_opt)
+  
+          
+        });
+       
+ } )
 }
 
 getStudents();
